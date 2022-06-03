75b8a3e0fe358ea07609ae8b34001650f2a30518
Sargis-Hovsepyan <sargishovsepyan2001@gmail.com>
2022-06-03 18:07:26 +0400

    Configuration of the database

--- srcs/services/mariadb/tools/configure.sh
+++ srcs/services/mariadb/tools/configure.sh
@@ -1,21 +1,26 @@
 #!/bin/sh
 
+# Creating environment for mysqld executable
 if [! -d "/run/mysqld"]; then
 	mkdir -p /run/mysqld
 	chown -R mysql:mysql /run/mysqld
 fi
 
+# Initializing DB and creating a table
 if [! -d "/var/lib/mysql/mysql"]; then
 
 	chown -R mysql:mysql /var/lib/mysql
 
+	# Installing db and creating mysql account
 	mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --rpm > /dev/null
 
+	# Creating a file to hold the commands to MariaDB
 	file=`config`
 	if [! -f "$file"]
 		return 1
 	fi
 
+	# Initializing the file with commands
 	cat << EOF > $file
 USE mysql;
 FLUSH PRIVILEGES;
@@ -34,10 +39,12 @@ GRANT ALL PRIVILEGES ON $WP_DATABASE_NAME.* TO '$WP_DTAABASE_USER'@'%';
 FLUSH PRIVILEGES;
 EOF
 
+	# Creating the environment
 	/usr/bin/mysqld --user=mysql --bootstrap < $file
 	rm -f $file
 fi
 
+# Allowing network connections
 sed -i "s|skip-networking|# skip-networking|g" /etc/my.cnf.d/mariadb-server.cnf
 sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/my.cnf.d/mariadb-server.cnf
 
