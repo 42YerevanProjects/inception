c0f59734f790808d24808d601dd81ed0fd3529eb
Sargis-Hovsepyan <sargishovsepyan2001@gmail.com>
2022-06-05 18:52:43 +0400

    Updated

--- srcs/services/mariadb/tools/configure.sh
+++ srcs/services/mariadb/tools/configure.sh
@@ -20,18 +20,14 @@ if [ ! -d "/var/lib/mysql/mysql" ]; then
 	cat << EOF > $file
 USE mysql;
 FLUSH PRIVILEGES;
-
 DELETE FROM	mysql.user WHERE User='';
 DROP DATABASE test;
 DELETE FROM mysql.db WHERE Db='test';
 DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
-
 ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
-
 CREATE DATABASE $WP_DATABASE_NAME CHARACTER SET utf8 COLLATE utf8_general_ci;
 CREATE USER '$WP_DATABASE_USER'@'%' IDENTIFIED by '$WP_DATABASE_PASSWORD';
 GRANT ALL PRIVILEGES ON $WP_DATABASE_NAME.* TO '$WP_DATABASE_USER'@'%';
-
 FLUSH PRIVILEGES;
 EOF
 	# Creating the environment
